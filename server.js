86cf128433afaba3023e256ae0b087891f456f90
ledoweb <ledoweb@gmail.com>
2017-11-27 19:43:13 +0100

    ---5

--- server.js
+++ server.js
@@ -18,18 +18,11 @@ app.use(express.static(__dirname + "/public"));
 //Register REST entry point
 //app.set("port", serverPort);
 
-
-
-
-
-
 //Using bodyParser to parse json requests, urlencoded is used for the "post" for the request of info
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-
 
 app.get("/getRichest", function(req, res) {
     const myQuery = sqlDb("donations");
@@ -46,7 +39,7 @@ app.post("/newPayment",function(req,res){
 app.get("/execute-payment", function(req, res){}); //This will be the entry point for after transaction complete */
 
 //Setting server port
-const serverPort = 8080;
+const serverPort = 12216;
 // Start the server on port 5000 
 app.listen(serverPort, function() {
     console.log(`Your app is ready at port ${serverPort}`);
